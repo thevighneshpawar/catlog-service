15c6e18147d8dde20e25728aed958c3e3b814d95
Vighnesh Pawar <vighneshpawar004@gmail.com>
2025-07-08 19:38:14 +0530

    add async wrapper

--- src/category/category.route.ts
+++ src/category/category.route.ts
@@ -3,12 +3,18 @@ import { CategoryController } from './category.controller';
 import categoryValidator from './categoryValidator';
 import { CategoryService } from './categoryService';
 import logger from '../config/logger';
+import { asyncWrapper } from '../common/utils/wrapper';
 
 const router = express.Router();
+
 const categoryservice = new CategoryService();
 
 const categoryController = new CategoryController(categoryservice, logger);
 
-router.post('/category', categoryValidator, categoryController.create);
+router.post(
+  '/category',
+  categoryValidator,
+  asyncWrapper(categoryController.create),
+);
 
 export default router;
